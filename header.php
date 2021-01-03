7ea42dbd5f59e973d5f3dd8841d29603706a8884
hendroagungs <hendroagungs@gmail.com>
2021-01-03 12:00:57 +0700

    perubahan navbar

--- header.php
+++ header.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Bungapedia</title>
-
-    <meta name="description" content="Source code generated using layoutit.com">
-    <meta name="author" content="LayoutIt!">
-
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<!-- <link href="css/bootstrap.css" rel="stylesheet"> -->
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="css/style.css" rel="stylesheet">
-	<link href="fontawesome/css/all.min.css" rel="stylesheet">
-	<link href="img/ikon.png" rel="icon">
-
-  </head>
-  <body>
-
-    <div class="container-fluid">
-	<div class="row">
+<div class="row">
 		<div class="col-md-12">
 			<div class="carousel slide" id="carousel-819037">
 				<ol class="carousel-indicators">
@@ -37,25 +13,25 @@
 					<div class="carousel-item active">
 						<img class="d-block w-100" alt="Carousel Bootstrap First" src="img/janda-bolong.png" height="250px">
 						<div class="carousel-caption">
-							<h3>
+							<h1 class="font1">
 								BUNGAPEDIA
-							</h3>
+							</h1>
 						</div>
 					</div>
 					<div class="carousel-item">
 						<img class="d-block w-100" alt="Carousel Bootstrap Second" src="img/spora.jpg" height="250px">
 						<div class="carousel-caption">
-							<h3>
+							<h1 class="font1">
 								BUNGAPEDIA
-							</h3>
+							</h1>
 						</div>
 					</div>
 					<div class="carousel-item">
 						<img class="d-block w-100" alt="Carousel Bootstrap Third" src="img/talas-putih.jpg" height="250px">
 						<div class="carousel-caption">
-							<h3>
+							<h1 class="font1">
 								BUNGAPEDIA
-							</h3>
+							</h1>
 						</div>
 					</div>
 				</div> <a class="carousel-control-prev" href="#carousel-819037" data-slide="prev"><span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span></a> <a class="carousel-control-next" href="#carousel-819037" data-slide="next"><span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
--- navbar.php
+++ navbar.php
@@ -1,39 +1,72 @@
-<div class="row">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bungapedia</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- <link href="css/bootstrap.css" rel="stylesheet"> -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="fontawesome/css/all.min.css" rel="stylesheet">
+	<link href="img/ikon.png" rel="icon">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
 		<div class="col-md-12">
-			<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-				 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="navbar-toggler-icon"></span>
-				</button> <a class="navbar-brand" href="#">Bungapedia</a>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="navbar-nav">
-						<li class="nav-item active">
-							 <a class="nav-link" href="#"><i class="fas fa-cart-plus"></i> <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item active">
-							 <a class="nav-link" href="#">Jenis <span class="sr-only">(current)</span></a>
-						</li>
-					</ul>
-					<form class="form-inline">
-						<input class="form-control mr-sm-2" type="text" placeholder="cari bunga"> 
-						<button class="btn btn-primary my-2 my-sm-0" type="submit">
-						<i class="fas fa-search-plus"></i>
-						</button>
-					</form>
-					<ul class="navbar-nav ml-md-auto">
-						<li class="nav-item active">
-							 <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item dropdown">
-							 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-								 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-								<div class="dropdown-divider">
-								</div> <a class="dropdown-item" href="#">Separated link</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="navbar-toggler-icon"></span>
+			</button> <a class="navbar-brand" href="#">Bungapedia</a>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						 <a class="nav-link" href="#">Jenis <span class="sr-only">(current)</span></a>
+					</li>
+				</ul>
+				<!-- TOMBOL SEARCH -->
+				<form class="form-inline">
+					<input class="form-control mr-sm-2" type="text" placeholder="cari bunga"> 
+					<button class="btn btn-primary my-2 my-sm-0" type="submit">
+					<i class="fas fa-search-plus"></i>
+					</button>
+				</form>
+				<!-- -->
+
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						 <a class="nav-link" href="#"><i class="fas fa-cart-plus"></i> <span class="sr-only">(current)</span></a>
+					</li>
+				</ul>
+
+				<ul class="navbar-nav ml-md-auto">
+					<li class="nav-item active">
+						 <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item dropdown">
+						 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+							 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+							<div class="dropdown-divider">
+							</div> <a class="dropdown-item" href="#">Separated link</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+		
 	</div>
