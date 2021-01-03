7cb626615ab4d5c1d9043113bd679a622ca66a27
Muhamad Sidik Sugiri <sugiri2020@gmail.com>
2021-01-03 10:21:03 +0700

    tambah fontawesome

--- navbar.php
+++ navbar.php
@@ -4,28 +4,20 @@
 				 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 					<span class="navbar-toggler-icon"></span>
-				</button> <a class="navbar-brand" href="#">Brand</a>
+				</button> <a class="navbar-brand" href="#">Bungapedia</a>
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="navbar-nav">
 						<li class="nav-item active">
-							 <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
+							 <a class="nav-link" href="#"><i class="fas fa-cart-plus"></i> <span class="sr-only">(current)</span></a>
 						</li>
-						<li class="nav-item">
-							 <a class="nav-link" href="#">Link</a>
-						</li>
-						<li class="nav-item dropdown">
-							 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-								<div class="dropdown-divider">
-								</div> <a class="dropdown-item" href="#">Separated link</a>
-							</div>
+						<li class="nav-item active">
+							 <a class="nav-link" href="#">About Us <span class="sr-only">(current)</span></a>
 						</li>
 					</ul>
 					<form class="form-inline">
 						<input class="form-control mr-sm-2" type="text"> 
 						<button class="btn btn-primary my-2 my-sm-0" type="submit">
-							Search
+						<i class="fas fa-search-plus"></i>
 						</button>
 					</form>
 					<ul class="navbar-nav ml-md-auto">
