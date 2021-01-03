0f1f8a7382a15d1573afb248519b47249a24e298
Muhamad Sidik Sugiri <sugiri2020@gmail.com>
2021-01-03 14:12:35 +0700

    tahap awal selesai banget

--- navbar.php
+++ navbar.php
@@ -1,6 +1,31 @@
-<div class="row">
-		<div class="col-md-12">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <title>Bungapedia</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- <link href="css/bootstrap.css" rel="stylesheet"> -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="fontawesome/css/all.min.css" rel="stylesheet">
+	<link href="img/ikon.png" rel="icon">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
 		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
 			
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
