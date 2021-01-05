0379a99094c11637eedadb452f39548b929b1783
hendroagungs <hendroagungs@gmail.com>
2021-01-05 23:09:32 +0700

    merubah size search dan merubah deskripsi bunga

--- card.php
+++ card.php
@@ -4,7 +4,7 @@
 				<img src="img/bunga1.jpg" class="card-img-top" alt="...">
 				<div class="card-body">
 					<h5 class="card-title">Bunga Hias</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<p class="card-text">Tulip merupakan nama genus untuk 100 spesies tumbuhan berbunga yang termasuk ke dalam keluarga Liliaceae. Tulip berasal dari Asia Tengah, tumbuh liar di kawasan pegunungan Pamir dan pegunungan Hindu Kush dan stepa di Kazakhstan. Negeri Belanda terkenal sebagai negeri bunga tulip.</p>
 					<a href="#" class="btn btn-primary">Buy</a>
 				</div>
 			</div>
@@ -13,8 +13,8 @@
 			<div class="card" style="width: 18rem;">
 				<img src="img/bunga-sepatu.jpg" class="card-img-top" alt="...">
 				<div class="card-body">
-					<h5 class="card-title">Bunga Sepau</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<h5 class="card-title">Bunga Sepatu</h5>
+					<p class="card-text">Kembang sepatu adalah tanaman semak suku Malvaceae yang berasal dari Asia Timur dan banyak ditanam sebagai tanaman hias di daerah tropis dan subtropis. Bunga besar, berwarna merah dan tidak berbau.</p>
 					<a href="#" class="btn btn-primary">Buy</a>
 				</div>
 			</div>
@@ -24,7 +24,7 @@
 				<img src="img/bunga2.jpg" class="card-img-top" alt="...">
 				<div class="card-body">
 					<h5 class="card-title">Bunga Matahari</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<p class="card-text">Bunga matahari adalah tumbuhan semusim dari suku kenikir-kenikiran yang populer, baik sebagai tanaman hias maupun tanaman penghasil minyak. Bunga tumbuhan ini sangat khas: besar, biasanya berwarna kuning terang, dengan kepala bunga yang besar.</p>
 					<a href="#" class="btn btn-primary">Buy</a>
 				</div>
 			</div>
@@ -34,7 +34,7 @@
 				<img src="img/bunga3.jpg" class="card-img-top" alt="...">
 				<div class="card-body">
 					<h5 class="card-title">Bucket Mawar</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<p class="card-text">Mawar adalah suatu jenis tanaman semak dari genus Rosa sekaligus nama bunga yang dihasilkan tanaman ini. Mawar liar terdiri dari 100 spesies lebih, kebanyakan tumbuh di belahan bumi utara yang berudara sejuk.</p>
 					<a href="#" class="btn btn-primary">Buy</a>
 				</div>
 			</div>
